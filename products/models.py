9fa17f3720f1c6f26bcb42ce02f43ef59ed58f72
Bimalesh Kumar Yadav <bimaleshk07@gmail.com>
2026-04-27 15:27:43 +0530

    feat: create Product and ProductImage database models

---
+++ products/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+import uuid
+from django.utils.text import slugify
+from vendors.models import Vendor
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering =["name"]
+        verbose_name_plural = "Categories"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name="products")
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="products")
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def is_in_stock(self):
+        return self.stock >0
+
+class ProductImage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+    image = models.ImageField(upload_to="product/images/")
+    is_primary = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+      ordering = ["-is_primary", "created_at"]
+
+      
+    def __str__(self):
+        return f"{self.product.name} - Image"

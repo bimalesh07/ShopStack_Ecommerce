0ba48b2638f2142eb85120324ff112b072bc9e63
Bimalesh <bimaleshk07@gmail.com>
2026-05-13 19:30:40 +0530

    fix entrypoint for cerley

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,19 +3,19 @@
 # Neon Cloud Database targeted. Skipping local wait logic...
 echo "Starting entrypoint script..."
 
-# 1. Collect static files
+#  Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput || echo "Static collection failed, but moving forward..."
 
-# 2. Run migrations
+#  Run migrations
 echo "Running migrations..."
 python manage.py migrate --noinput || echo "Migration failed, but keeping container alive..."
 
-# 3.Dono Backend aur Worker ke liye Celery aur Gunicorn saath mein chalayenge
-echo "Starting Celery Worker and Gunicorn Server..."
+# 3. RAM bachane ke liye concurrency limit lagayi
+echo "Starting Celery Worker (Limited) and Gunicorn Server..."
 
-# Celery ko background me chalane ke liya  (& lagana zaroori hai)
-python -m celery -A config worker --loglevel=info &
+# --concurrency=1 lagana sabse zaroori hai, varna RAM crash ho jayegi
+python -m celery -A config worker --loglevel=info --concurrency=1 &
 
-# Gunicorn ko main process bana kar chalayenge (Isse container zinda rahega aur Port detect hoga)
-exec gunicorn config.wsgi:application --bind 0.0.0.0:10000  
+# Gunicorn mein bhi workers kam rakho taaki RAM bache
+exec gunicorn config.wsgi:application --bind 0.0.0.0:10000 --workers 1 --threads 2

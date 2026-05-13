defb404661f0a315dc7ed0a8821bb778aa8c7591
Bimalesh <bimaleshk07@gmail.com>
2026-05-13 12:25:58 +0530

    Fix: Added default values for email config to pass build

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -265,19 +265,22 @@ else:
     MEDIA_ROOT = BASE_DIR / "media"
 
 
-#Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-# Cast is important for Port number integer and TLS boolean
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#add defult for render 
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+
+#
 EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)
 
 # Default from email
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='[EMAIL_ADDRESS]')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='ShopStack <shopstackworld@gamil.com>')
+
 
 
 #Razorpay Payment Gateway

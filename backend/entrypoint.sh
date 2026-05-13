a0400c90df969df2d7d4ed7bf109d8550549115e
Bimalesh <bimaleshk07@gmail.com>
2026-05-13 19:17:03 +0530

    feat: add entrypoint script to automate static file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,11 +11,11 @@ python manage.py collectstatic --noinput || echo "Static collection failed, but 
 echo "Running migrations..."
 python manage.py migrate --noinput || echo "Migration failed, but keeping container alive..."
 
-# 3. FIX: Dono Backend aur Worker ke liye Celery aur Gunicorn saath mein chalayenge
+# 3.Dono Backend aur Worker ke liye Celery aur Gunicorn saath mein chalayenge
 echo "Starting Celery Worker and Gunicorn Server..."
 
-# Celery ko background mein chalao (& lagana zaroori hai)
+# Celery ko background me chalane ke liya  (& lagana zaroori hai)
 python -m celery -A config worker --loglevel=info &
 
-# Gunicorn ko main process bana kar chalao (Isse container zinda rahega aur Port detect hoga)
-exec gunicorn config.wsgi:application --bind 0.0.0.0:10000
+# Gunicorn ko main process bana kar chalayenge (Isse container zinda rahega aur Port detect hoga)
+exec gunicorn config.wsgi:application --bind 0.0.0.0:10000  
